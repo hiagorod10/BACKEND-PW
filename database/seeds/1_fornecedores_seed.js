5fd5f120070c7312d2f0b5f097447831a4c44894
HiagoMendes <hiagorod123@gmail.com>
2021-12-10 20:38:16 -0300

    atualizacao

--- database/seeds/1_fornecedores_seed.js
+++ database/seeds/1_fornecedores_seed.js
@@ -1,7 +1,7 @@
 
 exports.seed = function(knex) {
   // Deletes ALL existing entries
-  return knex("fornecedores").del()
+  return knex("fornecedores")
     .then(function () {
       // Inserts seed entries
       return knex("fornecedores").insert([
